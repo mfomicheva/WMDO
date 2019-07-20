4be68b8383a1bd8ec1d14410bf47ded4da119747
mfomicheva <mfomicheva@at-languagesolutions.com>
2019-07-20 21:05:50 +0200

    Added tests

---
+++ WMDO/tests/test_wmdo.py
@@ -0,0 +1,28 @@
+import os
+
+from WMDO.tests.utils import create_vectors_file
+from WMDO.wmdo import load_wv
+from WMDO.wmdo import wmdo
+
+
+def test_computes_wmdo_same_languge():
+    vectors_file = create_vectors_file()
+    vectors = load_wv(vectors_file, binned=False)
+    os.remove(vectors_file)
+    test = 'test'
+    reference = 'test'
+    score = wmdo(vectors, test, reference, delta=0.18, alpha=0.10)
+    assert score == float('inf')
+    assert score
+
+
+def test_computes_wmdo_cross_lingual():
+    vectors_file = create_vectors_file()
+    vectors = load_wv(vectors_file, language='en', binned=False)
+    vectors = load_wv(vectors_file, existing=vectors, language='de', binned=False)
+    os.remove(vectors_file)
+    test = 'test'
+    reference = 'test'
+    score = wmdo(vectors, reference, test, ref_lang='en', cand_lang='de', delta=0.18, alpha=0.10)
+    assert score == float('inf')
+    assert score
